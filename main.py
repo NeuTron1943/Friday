c7d193b510cbd045eaf395dc5f6f7d3ebd73ae8c
NeuTron <kirill-galaktionov@mail.ru>
2022-02-21 15:00:42 +0300

    Refactoring. Audio is playing by bot.voice_client[0]

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ import asyncio
 from gtts import gTTS
 import os
 
+# read secret token from file :) conspiracy
 with open('token.txt') as inpf:
     token = inpf.readline()
+
 
 # client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
+
+# event occurs when bot logges into server
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
 
+
+# test command, writes arg in same chat
 @bot.command()
 async def q(ctx, arg):
     await ctx.send(arg)
 
+
+# join same voice channel, as the author of the command
 @bot.command()
 async def joinVoice(ctx):
     if (ctx.author.voice): # Check if author of the command is in voice channel
@@ -30,6 +38,8 @@ async def joinVoice(ctx):
     else:
         await ctx.send("Join voice channel to invite Friday")
 
+
+# leave current voice channel
 @bot.command()
 async def leaveVoice(ctx): # Note: ?leave won't work, only ?~ will work unless you change  `name = ["~"]` to `aliases = ["~"]` so both can work.
     if (ctx.voice_client): # If the bot is in a voice channel 
@@ -38,31 +48,43 @@ async def leaveVoice(ctx): # Note: ?leave won't work, only ?~ will work unless y
     else: # But if it isn't
         await ctx.send("I'm not in voice channel")
 
+
+# playing sound
+'''
 @bot.command()# TODO
 async def playSmth(ctx):
     await ctx.send('Attempting to play')
     audio_source = discord.FFmpegPCMAudio('content/audio/Ohayo.mp3')
-    if (ctx.voice_client):
-        ctx.voice_client.play(audio_source)
+    if (bot.voice_clients):
+        bot.voice_clients[0].play(audio_source)
+    else:
+        await ctx.send('Friday is not connected to voice channel')
+'''
 
 
+# pronouncing sentence of any length
 @bot.command()
 async def say(ctx, *args):
     voiceline = ''
     for arg in args:
         voiceline += str(arg)
         voiceline += " "
-    
     # await ctx.send('Saying')
     tmp_path = 'content/audio/tmp/voiceline.mp3'
     narrator = gTTS(text=voiceline, lang='ru', slow=False)
     narrator.save(tmp_path)
 
     audio_source = discord.FFmpegPCMAudio(tmp_path)
-    if (ctx.voice_client):
-        ctx.voice_client.play(audio_source)
+    # if (ctx.voice_client):
+        # ctx.voice_client.play(audio_source)
+    if (bot.voice_clients):
+        bot.voice_clients[0].play(audio_source)
+    else:
+        await ctx.send('Friday is not connected to voice channel')
+        
 
 
+# play Ohayo when someone entered same voice channel
 @bot.event
 async def on_voice_state_update(member, before, after):
     voiceClient = discord.utils.get(bot.voice_clients, guild=member.guild)
@@ -72,6 +94,7 @@ async def on_voice_state_update(member, before, after):
         voiceClient.play(audio_source)
                  
 
+# shutdown bot
 @bot.command()
 async def shutdown(ctx):
     if str(ctx.author.top_role) == "Кирилл":
@@ -80,5 +103,6 @@ async def shutdown(ctx):
     else:
         await ctx.send('Только Кирилл может меня выключить, позови его.')
 
-    
+
+# running bot
 bot.run(token)
